583dd762b13e069ee2b742ec17f4dedaf472dfba
KSentak <k.sentak@outlook.com>
2020-06-02 11:13:49 -0400

    added theme content to react site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SampleWork from './pages/SampleWork';
+import Contact from './pages/Contact';
+// import Error from './pages/Error';
+
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/transitions.css';
+import './assets/css/color.css';
+import './assets/css/circle.css';
+import './assets/css/main.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hey</h1>
-    </div>
+    <Router>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames='fade'>
+              <Switch location={location}>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/samplework' component={SampleWork} />
+                <Route path='/contact' component={Contact} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <section class='title-section text-left text-sm-center revealator-slideup revealator-once revealator-delay1'>
+        <h1>
+          CONTACT <span>MATT MURPHY</span>
+        </h1>
+      </section>
+      <section className='main-content revealator-slideup revealator-once revealator-delay1'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12 col-lg-4'>
+              <h3 className='text-uppercase custom-title mb-0 ft-wt-600 pb-3'>Don't be shy !</h3>
+              <p className='open-sans-font mb-3'>
+                Feel free to get in touch with me. I am always open to discussing new projects,
+                creative ideas or opportunities to be part of your visions.
+              </p>
+              <p className='open-sans-font custom-span-contact position-relative'>
+                <i className='fa fa-envelope-open position-absolute'></i>
+                <span className='d-block'>mail me</span>matt@murphy.com
+              </p>
+              <p className='open-sans-font custom-span-contact position-relative'>
+                <i className='fa fa-phone-square position-absolute'></i>
+                <span className='d-block'>call me</span>(888) 888-8888
+              </p>
+              <ul className='social list-unstyled pt-1 mb-5'>
+                <li className='facebook'>
+                  <a title='Facebook' href='#'>
+                    <i className='fa fa-facebook'></i>
+                  </a>
+                </li>
+                <li className='twitter'>
+                  <a title='Twitter' href='#'>
+                    <i className='fa fa-twitter'></i>
+                  </a>
+                </li>
+                <li className='youtube'>
+                  <a title='Youtube' href='#'>
+                    <i className='fa fa-youtube'></i>
+                  </a>
+                </li>
+                <li className='dribbble'>
+                  <a title='Dribbble' href='#'>
+                    <i className='fa fa-dribbble'></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
