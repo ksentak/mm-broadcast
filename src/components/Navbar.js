e751a4575bc56c0ad7971c453f8a27b48b405d38
KSentak <k.sentak@outlook.com>
2020-06-02 13:39:57 -0400

    add navlink to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
@@ -6,29 +7,33 @@ class Navbar extends Component {
       <div className='Navbar'>
         <header class='header' id='navbar-collapse-toggle'>
           <ul class='icon-menu d-none d-lg-block revealator-slideup revealator-once revealator-delay1'>
-            <li class='icon-box active'>
+            {/* Home */}
+            <li class='icon-box'>
               <i class='fa fa-home'></i>
-              <a href='index.html'>
+              <NavLink exact to='/' activeClassName='active'>
                 <h2>Home</h2>
-              </a>
+              </NavLink>
             </li>
+            {/* About */}
             <li class='icon-box'>
               <i class='fa fa-user'></i>
-              <a href='about.html'>
+              <NavLink exact to='/about' activeClassName='active'>
                 <h2>About</h2>
-              </a>
+              </NavLink>
             </li>
+            {/* Sample Work */}
             <li class='icon-box'>
               <i class='fa fa-briefcase'></i>
-              <a href='portfolio.html'>
-                <h2>Portfolio</h2>
-              </a>
+              <NavLink exact to='/samplework' activeClassName='active'>
+                <h2>Sample Work</h2>
+              </NavLink>
             </li>
+            {/* Contact */}
             <li class='icon-box'>
               <i class='fa fa-envelope-open'></i>
-              <a href='contact.html'>
+              <NavLink exact to='/contact' activeClassName='active'>
                 <h2>Contact</h2>
-              </a>
+              </NavLink>
             </li>
           </ul>
           <nav role='navigation' class='d-block d-lg-none'>
@@ -39,28 +44,28 @@ class Navbar extends Component {
               <span></span>
               <ul class='list-unstyled' id='menu'>
                 <li class='active'>
-                  <a href='index.html'>
+                  <NavLink exact to='/'>
                     <i class='fa fa-home'></i>
                     <span>Home</span>
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                  <a href='about.html'>
+                  <NavLink exact to='/about'>
                     <i class='fa fa-user'></i>
                     <span>About</span>
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                  <a href='portfolio.html'>
+                  <NavLink exact to='/sampleworks'>
                     <i class='fa fa-folder-open'></i>
-                    <span>Portfolio</span>
-                  </a>
+                    <span>Sample Works</span>
+                  </NavLink>
                 </li>
                 <li>
-                  <a href='contact.html'>
+                  <NavLink exact to='/contact'>
                     <i class='fa fa-envelope-open'></i>
                     <span>Contact</span>
-                  </a>
+                  </NavLink>
                 </li>
               </ul>
             </div>
