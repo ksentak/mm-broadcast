14a51cb12f191fa2c0eb1a5fa3e1de1f422a7f77
KSentak <k.sentak@outlook.com>
2020-06-02 13:19:22 -0400

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className='footer'>
+        <div className='col-sm-12 social-icons'>{/* Add Icons Here */}</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
